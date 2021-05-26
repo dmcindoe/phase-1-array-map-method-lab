8f640c84030ca1673f26ebc9efed49ae300d61de
Andrew McIndoe <amcindoe@powerhrg.com>
2021-05-26 13:24:17 -0400

    Map Function Lab

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
-}
+const titleCased = () => tutorials.map(tutorial => {
+  const splitWords = tutorial.split(' ')
+  const firstLetter = splitWords.map(word => {
+    return word.charAt(0).toUpperCase() + word.slice(1)
+})
+    return firstLetter.join(' ') 
+}) 
